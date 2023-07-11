9cb78f2cbfbce76722bcc3d3bedc29bfe183cec7
Eliza Oser <oser.18@osu.edu>
2023-07-11 14:30:41 +0000

    R Linear Regression Files and PNGs

---
+++ R_Linear.r
@@ -0,0 +1,51 @@
+filename = "regrex1.csv"
+
+dataset = read.csv(filename)
+dataset
+
+png("r_orig.png")
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  ggtitle('y vs x') +
+  xlab('x') +
+  ylab('y')
+dev.off()
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  ggtitle('y vs x') +
+  xlab('x') +
+  ylab('y')
+
+model = lm(formula = y ~ x,
+               data = dataset)
+
+png("r_lm.png")
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('y vs x') +
+  xlab('x') +
+  ylab('y')
+dev.off()
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('y vs x') +
+  xlab('x') +
+  ylab('y')
+
+summary(model)
+
+
